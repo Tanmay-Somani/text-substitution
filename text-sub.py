46e45cc18298964a9753aa7001826ed193658055
Tanmay Somani <tanmaysomani2003@gmail.com>
2023-08-10 13:26:38 +0530

    Update text-sub.py

--- text-sub.py
+++ text-sub.py
@@ -8,12 +8,16 @@ text_substitutions = {
 }
 
 def expand_text(e):
-    if e.event_type == keyboard.KEY_DOWN and e.name == "space":
-        text = keyboard.get_typed_strings(e)
-        for trigger, replacement in text_substitutions.items():
-            if trigger in text:
-                expanded_text = text.replace(trigger, replacement)
-                keyboard.write(expanded_text) #here is the code via which you expand your text by replacing by performing your certain tasks
+  global typed_chars
+  if e.event_type == keyboard.KEY_DOWN:
+    if e.name == "space":
+      for trigger, replacement in text_substitutions.items():
+        if trigger in typed_chars:
+          typed_chars = typed_chars.replace(trigger, replacement)
+          keyboard.write(typed_chars)
+          typed_chars = ""
+        else:
+            typed_chars += e.name #here is the code via which you expand your text by replacing by performing your certain tasks
 
 # Hooking the keyboard event
 keyboard.hook(expand_text)
